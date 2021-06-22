81869dc60018ae8419795e773108ecdf9cd1a99f
wzhang-PROX14 <zhangwentaoucas@gmail.com>
2021-06-23 01:03:46 +0200

    some codes

--- PyLib/main.py
+++ PyLib/main.py
@@ -11,6 +11,8 @@ Created on Thu Jun 17 18:14:31 2021
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import linalg
+
 import scipy.io as scio
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -21,8 +23,18 @@ from pylab import rcParams
 LitModOutputName='post_processing_output.dat';
 LitMod=np.loadtxt(LitModOutputName);
 
-dataFile = '/home/ictja/Music/stokes/120km2/Air120km_Step9_stokes.mat'
+
+
+# Solve matrix
+# S=L\R;
+dataFile = '/home/wzhang/ownCloud/Stoke/CH21_online/saveA.mat'
 # dataFile = '/home/ictja/Music/stokes/120km2/Air120km_Step9.mat'
 data = scio.loadmat(dataFile)
 
-L = 
+
+L = data['L']
+R = data['R']
+S = data['S']
+
+XX = linalg.solve(L, R)
+# x = linalg.solve(a, b)
