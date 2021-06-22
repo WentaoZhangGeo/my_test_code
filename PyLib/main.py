57f314b172dfd6c1ae0c2aa7b7bfa33dd16b71b8
wzhangICTJA <zhangwentaoucas@gmail.com>
2021-06-22 19:33:46 +0200

    Some codes.

---
+++ PyLib/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 17 18:14:31 2021
+
+@author: wzhang
+"""
+       
+
+# Cell-Particles Projections
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.io as scio
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+from pylab import rcParams
+
+LitModOutputName='post_processing_output.dat';
+LitMod=np.loadtxt(LitModOutputName);
+
+dataFile = '/home/ictja/Music/stokes/120km2/Air120km_Step9_stokes.mat'
+# dataFile = '/home/ictja/Music/stokes/120km2/Air120km_Step9.mat'
+data = scio.loadmat(dataFile)
+
+L = 
