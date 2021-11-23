1f19ee1f0dd2a30f20f357c78e9bb1aaf0426fa6
wzhangICTJA <zhangwentaoucas@gmail.com>
2021-11-23 15:44:24 +0100

    caption2word.py

--- README.md
+++ README.md
@@ -14,16 +14,18 @@ github常见命令:
 git config --list
 # 编辑Git配置文件
 git config -e [--global]
+# 设置提交代码时的用户信息
+git config --global user.name "w**"
+git config --global user.email "z******.com@gmail.com"
+
 
 # 下载一个项目和它的整个代码历史
 git clone https://github.com/wzhang1994/my_test_code
 # 检查是否有变更的文件
 cd $Repository_dir$ # 进入仓库的目录
 git status
-
-# 设置提交代码时的用户信息
-git config --global user.name "w**"
-git config --global user.email "z******.com@gmail.com"
+# 更新最新的代码
+git pull 
 
 # 新文件提交到缓冲区，但注意并没有提交到远程仓库
 git add .
---
+++ caption2word.py
@@ -0,0 +1,34 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Description
+caption2word reads one or more caption files and writes out.
+
+Convert caption to word for printing
+
+Synopsis
+python caption2word 
+
+Note: The caption comes from Mooc. 
+
+@author: wzhang
+Created on Tue Nov 23 11:36:14 2021
+"""
+def caption2word(file_in, file_out):
+    file_data = ''
+    with open(file_in, 'r') as f:
+        for (num, line) in enumerate(f):
+            if line[0:2]!='00' and line!='\n' and line[0:2].isdigit()==False:
+                file_data += line[:-2] + ' '
+    
+    with open(file_out, 'w') as f:
+        f.write(file_data)
+
+
+# file_in='/home/ictja/Downloads/Mooc_Downloader-master/Mooc/大学英语综合课程(一)__大学英语综合课程(一)__国防科技大学/{1}--课程/{1}--Week1UnitOneStartingOut/{1}--Overview/[1.1.1]--overview.srt'
+print('Input file name:')
+file_in=input()
+file_out=file_in[:-3] + 'txt'
+
+caption2word(file_in, file_out)
+print('OK!')
